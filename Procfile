2b8f5c0e5a61cf8f6f3042119da77aaa50d4a128
Chap Ambrose <chapambrose@gmail.com>
2020-01-08 14:44:19 +0000

    add release

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: ruby app.rb
+release: echo 'hello'
--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require "sinatra"
 
 get '/*' do
   puts params.to_s
-  return params.to_s
+  return "release-should-work:#{params}"
 end
 
 post '/*' do
